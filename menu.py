07559a565e14574d00db6b285940966183d2099b
Adhavane <119078555+Adhavane@users.noreply.github.com>
2023-07-27 01:50:17 +0200

    Update menu display

--- menu.py
+++ menu.py
@@ -28,9 +28,9 @@ class MenuState(State):
         scale = settings.CREDIT_HEIGHT / self.credit.get_height()
         self.credit = pygame.transform.scale(self.credit, (int(self.credit.get_width() * scale), int(self.credit.get_height() * scale)))
 
-        self.__credit_rect = self.credit.get_rect()
-        self.__credit_rect.x = self.title_rect.x + self.title_rect.width - self.__credit_rect.width
-        self.__credit_rect.y = settings.CREDIT_Y
+        self.credit_rect = self.credit.get_rect()
+        self.credit_rect.x = self.title_rect.x + self.title_rect.width - self.credit_rect.width
+        self.credit_rect.y = settings.CREDIT_Y
 
         self.button_play = ButtonPlay(self.game)
         self.button_quit = ButtonQuit(self.game)
@@ -39,9 +39,11 @@ class MenuState(State):
         self.buttons.add(self.button_play)
         self.buttons.add(self.button_quit)
 
+        self.button_play.change_state("selected")
+
     def update(self) -> None:
         super().update()
-
+        
         self.buttons.update()
 
     def draw(self) -> None:
@@ -50,4 +52,4 @@ class MenuState(State):
         self.game.screen.blit(self.title, self.title_rect)
         for button in self.buttons:
             button.draw(self.game.screen)
-        self.game.screen.blit(self.credit, self.__credit_rect)
+        self.game.screen.blit(self.credit, self.credit_rect)
