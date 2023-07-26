588d4a1a6c7af8533ecc147cba1e149570b0d69a
Adhavane <119078555+Adhavane@users.noreply.github.com>
2023-07-26 20:33:43 +0200

    Update state.py

--- play.py
+++ play.py
@@ -9,24 +9,12 @@ from settings import *
 settings = Settings()
 
 from game import State
-from background import Background
-from cloud import Cloud
 from player import Player
 from enemy import EnemyPurple
 
 class PlayState(State):
     def __init__(self, game) -> None:
         super().__init__(game)
-
-        self.background = Background()
-        
-        self.clouds = pygame.sprite.Group()
-        for _ in range(settings.CLOUD_INIT):
-            cloud = Cloud()
-            cloud.rect.x = random.randint(0, settings.WIDTH)
-            self.clouds.add(cloud)
-        self.cloud_timer = 0
-        self.cloud_cooldown = random.randint(settings.CLOUD_TIME_MIN, settings.CLOUD_TIME_MAX)
 
         self.player = Player()
 
@@ -36,10 +24,7 @@ class PlayState(State):
         self.enemies = pygame.sprite.Group()
 
     def update(self) -> None:
-        self.background.update()
-
-        self.spawn_clouds()
-        self.clouds.update()
+        super().update()
 
         self.player.update()
 
@@ -47,14 +32,6 @@ class PlayState(State):
         self.enemies.update(self.player)
 
         self.collisions()
-
-    def spawn_clouds(self) -> None:
-        current_time = pygame.time.get_ticks()
-        if current_time - self.cloud_timer > self.cloud_cooldown:
-            self.cloud_timer = current_time
-            self.cloud_cooldown = random.randint(settings.CLOUD_TIME_MIN, settings.CLOUD_TIME_MAX)
-            cloud = Cloud()
-            self.clouds.add(cloud)
     
     def spawn_enemies(self) -> None:
         current_time = pygame.time.get_ticks()
--- state.py
+++ state.py
@@ -2,8 +2,40 @@
 
 """state.py: State class."""
 
+import pygame
+import random
 from abc import ABC, abstractmethod
+
+from settings import *
+settings = Settings()
+
+from background import Background
+from cloud import Cloud
 
 class State(ABC):
     def __init__(self, game) -> None:
         self.game = game
+
+        self.background = Background()
+
+        self.clouds = pygame.sprite.Group()
+        for _ in range(settings.CLOUD_INIT):
+            cloud = Cloud()
+            cloud.rect.x = random.randint(0, settings.WIDTH)
+            self.clouds.add(cloud)
+        self.cloud_timer = 0
+        self.cloud_cooldown = random.randint(settings.CLOUD_TIME_MIN, settings.CLOUD_TIME_MAX)
+
+    def spawn_clouds(self) -> None:
+        current_time = pygame.time.get_ticks()
+        if current_time - self.cloud_timer > self.cloud_cooldown:
+            self.cloud_timer = current_time
+            self.cloud_cooldown = random.randint(settings.CLOUD_TIME_MIN, settings.CLOUD_TIME_MAX)
+            cloud = Cloud()
+            self.clouds.add(cloud)
+
+    def update(self) -> None:
+        self.background.update()
+        
+        self.spawn_clouds()
+        self.clouds.update()
