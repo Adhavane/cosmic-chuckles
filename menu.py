0901d6220eb0e086bb240c5d988a93bb26d69eb9
Adhavane <119078555+Adhavane@users.noreply.github.com>
2023-07-26 20:35:37 +0200

    Update state.py

--- menu.py
+++ menu.py
@@ -14,15 +14,19 @@ class MenuState(State):
     def __init__(self, game) -> None:
         super().__init__(game)
 
-        self.background = Background()
-
         self.title = pygame.image.load(settings.TITLE_IMG).convert_alpha()
+        
+        scale = settings.TITLE_HEIGHT / self.title.get_height()
+        self.title = pygame.transform.scale(self.title, (int(self.title.get_width() * scale), int(self.title.get_height() * scale)))
+
+        self.title_rect = self.title.get_rect()
+        self.title_rect.center = (settings.WIDTH / 2, settings.HEIGHT / 2)
+
 
     def update(self) -> None:
-        self.background.update()
+        super().update()
 
     def draw(self) -> None:
         self.game.screen.fill(settings.BLACK)
 
-        self.background.draw(self.game.screen)
-        
+        self.game.screen.blit(self.title, self.title_rect)
