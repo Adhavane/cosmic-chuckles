00b6ae16b55fdc08092740d29255d2c11db04888
Adhavane <119078555+Adhavane@users.noreply.github.com>
2023-07-27 18:33:05 +0200

    Update particle display

--- particle.py
+++ particle.py
@@ -15,17 +15,19 @@ class Particle(Projectile):
         angle_min = 0
         angle_max = 360
         speed_min = 1
-        speed_max = 3
+        speed_max = 5
         damage_min = 0
         damage_max = 0
         lifetime_min = 10
-        lifetime_max = 20
+        lifetime_max = 60
 
         angle = random.randint(angle_min, angle_max)
         speed = random.randint(speed_min, speed_max)
         damage = random.randint(damage_min, damage_max)
         lifetime = random.randint(lifetime_min, lifetime_max)
 
-        super().__init__(settings.PARTICLE_IMG, x, y, angle, speed, damage, lifetime)
+        super().__init__(settings.PARTICLE_IMG,
+                         x, y, settings.PARTICLE_HEIGHT,
+                         angle, speed, damage, lifetime)
 
         self.image.fill(color)
