de318980a83742c82a4e01283678e1533f65c78b
Adhavane <119078555+Adhavane@users.noreply.github.com>
2023-08-11 14:30:02 +0200

    Update transition animation

--- src/scenes/transition.py
+++ src/scenes/transition.py
@@ -5,6 +5,7 @@
 from __future__ import annotations
 
 import pygame
+from typing import Tuple
 
 from src.settings import Settings
 settings = Settings()
@@ -18,9 +19,9 @@ class TransitionState(State):
         self.start_time: int = pygame.time.get_ticks()
         self.end_time: int = self.start_time + execution_time
 
-        self.circle_radius: int = 0
-        self.circle_color: tuple = settings.BLACK
-        self.circle_center: tuple = (settings.SCREEN_WIDTH / 2, settings.SCREEN_HEIGHT / 2)
+        self.circle_radius: int = 5
+        self.circle_color: Tuple = settings.RED
+        self.circle_center: Tuple = (settings.SCREEN_WIDTH / 2, settings.SCREEN_HEIGHT / 2)
         self.circle: pygame.Surface = pygame.Surface((self.circle_radius * 2, self.circle_radius * 2))
         self.circle_rect: pygame.Rect = self.circle.get_rect()
         self.circle_rect.center = self.circle_center
@@ -33,18 +34,18 @@ class TransitionState(State):
 
         current_time: int = pygame.time.get_ticks()
         if current_time >= self.end_time:
-            self.game.change_state(self.next_state, *self.next_state_args, **self.next_state_kwargs)
+            self.game.next_state()
 
-        self.circle_radius = round((current_time - self.start_time) / (self.end_time - self.start_time) * settings.SCREEN_WIDTH / 2)
+        self.circle_radius += 30 * Settings.DELTA_TIME
         self.circle = pygame.Surface((self.circle_radius * 2, self.circle_radius * 2))
         self.circle_rect = self.circle.get_rect()
         self.circle_rect.center = self.circle_center
 
     def draw(self) -> None:
         super().draw()
-        
+
         pygame.draw.circle(self.circle, self.circle_color, (self.circle_radius, self.circle_radius), self.circle_radius)
-        # self.game.screen.blit(self.circle, self.circle_rect)
-        self.game.screen.blit(self.circle, self.circle_rect, special_flags=pygame.BLEND_RGBA_ADD)
+        self.game.display.blit(self.circle, self.circle_rect)
+        self.game.display.blit(self.circle, self.circle_rect, special_flags=pygame.BLEND_RGBA_ADD)
     
 from src.game import Game
