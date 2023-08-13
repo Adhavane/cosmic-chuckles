9b03a5914ff92b2d17cfde399caef0eea518aa1f
Adhavane <119078555+Adhavane@users.noreply.github.com>
2023-08-13 20:47:43 +0200

    Update transition effect

--- src/scenes/transition.py
+++ src/scenes/transition.py
@@ -5,7 +5,7 @@
 from __future__ import annotations
 
 import pygame
-from typing import Tuple
+from typing import Callable, Tuple
 import random
 import math
 
@@ -16,7 +16,8 @@ from src.scenes.state import State
 from src.game import Game
 
 class TransitionState(State):
-    def __init__(self, game: Game, execution_time: int) -> None:
+    def __init__(self, game: Game, script: Callable,
+                 execution_time: int) -> None:
         super().__init__(game)
 
         self.execution_time: int = execution_time
@@ -25,14 +26,12 @@ class TransitionState(State):
         self.square_size = math.ceil(settings.SCREEN_WIDTH / self.grid_size)  # Size of each square
         
         self.colors = []  # List to store random colors for each square
-        # Generate random colors for each square
         for _ in range(self.grid_size):
             row_colors = []
             for _ in range(self.grid_size):
-                color = [255, 0, 0, 0]
-                row_colors.append(color)
+                row_colors.append(settings.BLACK)
             self.colors.append(row_colors)
-        
+
         self.squares = []  # List to store the squares
         self.squares_unfilled = []  # List to store the unfilled squares
         for y in range(self.grid_size):
@@ -45,7 +44,8 @@ class TransitionState(State):
                 square_rect.y = y * self.square_size
                 self.squares.append((square, square_rect))
                 self.squares_unfilled.append((square, square_rect))
-
+        
+        self.script = script
         self.fill_rate = round(self.grid_size * self.grid_size / self.execution_time * settings.FPS)
 
     def events(self, _: pygame.event.Event) -> None:
@@ -54,13 +54,22 @@ class TransitionState(State):
     def update(self) -> None:
         super().update()
 
+        self.script()
+
+    def fade_in(self) -> None:
+        self.fade(255)
+
+    def fade_out(self) -> None:
+        self.fade(0)
+
+    def fade(self, opacity: int) -> None:
         # Fill the grid with random squares
         for _ in range(self.fill_rate):
             if len(self.squares_unfilled) > 0:
                 square = self.squares_unfilled.pop(random.randrange(len(self.squares_unfilled)))
                 y_index = square[1].y // self.square_size
                 x_index = square[1].x // self.square_size
-                self.colors[y_index][x_index][3] = 255
+                self.colors[y_index][x_index][3] = opacity
                 square[0].fill(self.colors[y_index][x_index])
                 square[0].set_alpha(self.colors[y_index][x_index][3])
 
